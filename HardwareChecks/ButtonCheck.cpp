3ab2d7054ee67f791c3bfb4a8947101d8f2ece3a
Alakshendra Singh <71720527+Alakshendra99@users.noreply.github.com>
2023-12-28 15:51:10 +0530

    Update ButtonCheck.cpp

--- HardwareChecks/ButtonCheck.cpp
+++ HardwareChecks/ButtonCheck.cpp
@@ -1,5 +1,11 @@
 #include <S32K146.h>
 
+#define LEDG 16		// PORT D : PTD16
+#define LEDB 0		// PORT D : PTD0
+#define LEDR 15		// PORT D : PTD15
+#define BTN1 12		// PORT C : PTC12
+#define BTN2 13		// PORT C : PTC13
+
 void DisableWatchDog(){
 	WDOG->CNT = 0xD928C520;
 	WDOG->TOVAL = 0x0000FFFF;
@@ -9,5 +15,40 @@ void DisableWatchDog(){
 void PORTSetup(){
 	PCC->nPCC[74]=0x40000000;
 	PCC->nPCC[75]=0x40000000;
-	
+	PTC->PDDR &= ~(1<<BTN1 | 1<<BTN2);
+	PTD->PDDR |= (1<<LEDR | 1<<LEDG | 1<<LEDB);
+
+	PORTC->PCR[BTN1] = 0x00000110;
+	PORTC->PCR[BTN2] = 0x00000110;
+	PORTD->PCR[LEDR] = 0x00000100;
+	PORTD->PCR[LEDG] = 0x00000100;
+	PORTD->PCR[LEDB] = 0x00000100;
+}
+
+int main(){
+	DisableWatchDog();
+	PORTSetup();
+	while(1){
+		if(PTC->PDIR & 1<<BTN1){
+			PTD->PCOR |= 1<<LEDG;
+			Flag1 = 1;
+		} else{
+			PTD->PSOR |= 1<<LEDG;
+			Flag1 = 0;
+		}
+
+		if(PTC->PDIR & 1<<BTN1){
+			PTD->PCOR |= 1<<LEDB;
+			Flag2 = 1;
+		} else{
+		PTD->PSOR |= 1<<LEDB;
+		Flag2 = 0;
+		}
+
+		if(Flag1 & Flag2){
+			PTD->PSOR |= 1<<LEDR;
+		} else{
+			PTD->PCOR |= 1<<LEDR;
+		}
+	}
 }
