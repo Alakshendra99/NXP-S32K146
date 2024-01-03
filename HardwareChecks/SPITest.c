b5ca827433971aaa51398a27e30cabdf48199b56
Alakshendra Singh <71720527+Alakshendra99@users.noreply.github.com>
2024-01-03 13:18:12 +0530

    Update SPITest.c

--- HardwareChecks/SPITest.c
+++ HardwareChecks/SPITest.c
@@ -1,5 +1,18 @@
 void DisableWatchDog(){
-	WDOG->CNT = 0xD928C520;
-	WDOG->TOVAL = 0x0000FFFF;
-	WDOG->CS = 0x00002100;
+	WDOG->CNT = 0xD928C520;				/*- Unlocking Sequence of Watchdog -*/
+	WDOG->TOVAL = 0x0000FFFF;			/*- Setting Maximum Value of Watchdog -*/
+	WDOG->CS = 0x00002100;				/*- Disabling Watchdog Timer -*/
 }
+void ClocksSOSC(){
+	SCG->SOSCDIV = 0x00000101;			/*- System Clock Dividers -*/
+	SCG->SOSCCFG = 0x00000024;			/*- System Clock Selector -*/
+
+	while(SCG->SOSCCSR & 0x00800000){		/*- Checking If SOSCCSR is Unlocked -*/
+		SCG->SOSCCSR &= ~(0x00800000);		/*- Unlocking SOSCCSR -*/
+	}
+
+	SCG->SOSCCSR = 0x00000001;			/*- System Clock Enabled-*/
+	while(SCG->SOSCCSR & 0x01000000);		/*- System Clock Validation -*/
+}
+
+	
